247d174788a14a11e99235db9a65dddf583849cb
João <joaotf159@gmail.com>
2020-03-11 17:18:16 -0300

    Ta pronto

--- geometry.py
+++ geometry.py
@@ -1,50 +1,120 @@
 # sudo python3 -mpip install -U PySimpleGUI
 # sudo apt-get install python3-tk 
-# sudo apt-get install matplotlib
+# pip3 install matplotlib
 
 import matplotlib.pyplot as plt
 import PySimpleGUI as sg
-import pandas 
+import numpy as np
+import math
 
-# Pontos da Casa
-eixo_x = [2,2,1,2,2.5,2.5,4,4,6.5,7.5,6.5,6.5,2]
-eixo_y = [2.5,4.5,4.5,6,6,7,7,6,6,4.5,4.5,2.5,2.5]
+pontos = np.array([
+    [2,2,1,2,2.5,2.5,4,4,6.5,7.5,6.5,6.5,2], # X
+    [2.5,4.5,4.5,6,6,7,7,6,6,4.5,4.5,2.5,2.5] # Y
+])
 
-
-def Plot(resize):
-
-    plt.plot(eixo_x,eixo_y,color='purple')
-    plt.scatter(eixo_x,eixo_y,color='blue')
+def Plot():
+    plt.plot(pontos[0],pontos[1],color='purple')
+    plt.scatter(pontos[0],pontos[1],color='blue')
     plt.show()
 
 sg.theme('DarkBlue3')   
 
 layout = [  
             [sg.Text('Menu')],
-            [sg.Button('Adicionar Ponto (X,Y)')],
-            [sg.Button('Remover Ponto (X,Y)')],
-            [sg.Button('Atualizar Ponto (X,Y)')],
+            [sg.Button('Translação - mover o objeto no eixo X e/ou em Y')],
+            [sg.Button('Escala - alterar escala do objeto em X e/ou Y')],
+            [sg.Button('Rotação - rotacionar objeto no sentido horário e/ou anti-horário')],
+            [sg.Button('Espelhamento - espelhar o polígono em X e/ou Y')],
+            [sg.Button('Cisalhamento - aplicar deformação em X e/ou Y')],
             [sg.Button('Visualizar Pontos')],
             [sg.Button('Sair')],
         ]
 
-window = sg.Window('Computação Gráfica', layout,size=(300,250))
+window = sg.Window('Computação Gráfica', layout,size=(500,300))
 while True:
     event, values = window.read()
     if(event in (None, 'Sair')):
         break
-    if(event == "Adicionar Ponto (X,Y)"):
+
+    if(event == "Translação - mover o objeto no eixo X e/ou em Y"):
+        quantidade = ''
+        eixo = ''    
         try:
-            eixo_x = int(sg.popup_get_text("Digite o eixo X"))
-            if(eixo_x == None):
-                eixo_x = int(sg.popup_get_text("Digite o eixo X"))
-            eixo_y = int(sg.popup_get_text("Digite o eixo Y"))
-            if(eixo_y == None):
-                eixo_y = int(sg.popup_get_text("Digite o eixo Y"))
-        except TypeError as identifier:
-            sg.popup("Entrada inválida")
+            quantidade = sg.PopupGetText("Digite a quantidade de passos")
+            eixo = sg.PopupGetText("Digite o Eixo\nX\nY\nXY",title="Trabalho Louco do Casseb")
+            if(str(eixo).upper() == "X"):
+                for i in range(len(pontos[0])):
+                    pontos[0][i] += int(quantidade)
+            if(str(eixo).upper() == "Y"):
+                for j in range(len(pontos[1])):
+                    pontos[1][j] += int(quantidade)
+            if(str(eixo).upper() == "XY"):
+                for k in range(len(pontos[0])):
+                    pontos[0][k] += int(quantidade)
+                    pontos[1][k] += int(quantidade)
+            Plot()
         
+        except TypeError as identifier:
+            pass
+
+    if(event == "Escala - alterar escala do objeto em X e/ou Y"):
+        multiplo = ''
+        eixo = ''    
+        try:
+            multiplo = sg.PopupGetText("Digite o Múltiplo Escalar")
+            eixo = sg.PopupGetText("Digite o Eixo\nX\nY\nXY",title="Trabalho Louco do Casseb")
+            if(str(eixo).upper() == "X"):
+                for i in range(len(pontos[0])):
+                    pontos[0][i] *= int(multiplo)
+            if(str(eixo).upper() == "Y"):
+                for j in range(len(pontos[1])):
+                    pontos[1][j] *= int(multiplo)
+            if(str(eixo).upper() == "XY"):
+                for k in range(len(pontos[0])):
+                    pontos[0][k] *= int(multiplo)
+                    pontos[1][k] *= int(multiplo)
+            Plot()
+        
+        except TypeError as identifier:
+            pass
+
+    if(event == "Espelhamento - espelhar o polígono em X e/ou Y"):
+        eixo = ''    
+        try:
+            eixo = sg.PopupGetText("Digite o Eixo\nX\nY\nXY",title="Trabalho Louco do Casseb")
+            if(str(eixo).upper() == "X"):
+                for i in range(len(pontos[0])):
+                    pontos[0][i] *= -1;
+            if(str(eixo).upper() == "Y"):
+                for j in range(len(pontos[1])):
+                    pontos[1][j] *= -1;
+            if(str(eixo).upper() == "XY"):
+                for k in range(len(pontos[0])):
+                    pontos[0][k] *= -1;
+                    pontos[1][k] *= -1;
+            Plot()
+        
+        except TypeError as identifier:
+            pass
+
+    if(event == "Rotação - rotacionar objeto no sentido horário e/ou anti-horário"):
+        sentido = ''    
+        try:
+            eixo = sg.PopupGetText("Digite o Sentido da Rotação",title="Trabalho Louco do Casseb")
+            grau = sg.PopupGetText("Graus",title="Trabalho Louco do Casseb")
+            if(str(eixo).upper() == "H"):
+                for i in range(len(pontos[0])):
+                    pontos[0][i] = float(pontos[0][i]) * math.cos(float(grau)) + float(pontos[1][i]) * math.sin(float(grau))
+                Plot()
+            if(str(eixo).upper() == "AH"):
+                for i in range(len(pontos[1])):
+                    pontos[0][i] = float(pontos[0][i]) * math.cos(float(grau)) - float(pontos[1][i]) * math.sin(float(grau))
+                Plot()
+            
+        except TypeError as identifier:
+            pass
+    
     if(event == "Visualizar Pontos"):
-        Plot(1)
+        Plot()
 
 window.close()
